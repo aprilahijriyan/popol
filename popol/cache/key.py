674bc30ad92d086cdd37edbb986f76c26d2e25e8
aprilahijriyan <hijriyan23@gmail.com>
2022-03-12 15:45:25 +0700

    Fix get app settings in get_cache_key()

--- popol/cache/key.py
+++ popol/cache/key.py
@@ -2,13 +2,16 @@ from hashlib import md5
 
 from fastapi import FastAPI, Request
 
+from popol.utils import get_settings
+
 
 def get_cache_key(request: Request) -> str:
     path = request.url.path
     query_params = request.scope.get("query_string", b"").decode()
     part = path + query_params
     app: FastAPI = request.app
-    if app.get_setting("CACHE_INCLUDE_IP_ADDRESS", True):
+    settings = get_settings(app)
+    if getattr(settings, "CACHE_INCLUDE_IP_ADDRESS", True):
         ip_address = request.client.host
         if ip_address:
             part += ip_address
