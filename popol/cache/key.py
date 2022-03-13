933107cadfcf056dc6695c3ce3056d9a52adb783
aprilahijriyan <hijriyan23@gmail.com>
2022-03-13 18:00:09 +0700

    Fix: insert http method to cache key

--- popol/cache/key.py
+++ popol/cache/key.py
@@ -8,11 +8,15 @@ from popol.utils import get_settings
 def get_cache_key(request: Request) -> str:
     """
     Generate cache keys using the concatenation of route paths, HTTP methods and query params.
+    And also available `CACHE_INCLUDE_IP_ADDRESS` configuration to insert client ip address to key.
+
+    Returns:
+        str: The cache key (md5 hash).
     """
 
     path = request.url.path
     query_params = request.scope.get("query_string", b"").decode()
-    part = path + query_params
+    part = path + request.method + query_params
     app: FastAPI = request.app
     settings = get_settings(app)
     if getattr(settings, "CACHE_INCLUDE_IP_ADDRESS", True):
