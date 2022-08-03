48af29ccf475c2237e87cdfc846eeee0897d16fd
aprilahijriyan <hijriyan23@gmail.com>
2022-08-03 19:06:08 +0700

    📃 docs: Adding title and description to sample app

--- examples/app/main.py
+++ examples/app/main.py
@@ -6,7 +6,7 @@ from app.middleware import init_middleware
 from app.routers import init_routers
 from app.settings import init_settings
 
-app = FastAPI()
+app = FastAPI(title="Popol", description="Demo API")
 init_settings(app)
 init_extensions(app)
 init_middleware(app)
